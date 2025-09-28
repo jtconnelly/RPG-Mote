4eec968210cd1a869a70d37b4043d36fedcba8e7
jtconnelly <jtc20659@gmail.com>
2025-09-28 19:36:05 -0400

    Got C++ Generator working

--- PluginLibs/C++/CppClassGenerator/CppClassGenerator.cs
+++ PluginLibs/C++/CppClassGenerator/CppClassGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Specialized;
+using System.Dynamic;
 using System.Net.Sockets;
 using System.Text.Json;
 
@@ -12,21 +13,23 @@ internal struct Method
 }
 public class CppClassGenerator : LanguageInterface
 {
-    internal Dictionary<string, string> valueConversions = new Dictionary<string, string>
+    internal Dictionary<String, String> valueConversions = new Dictionary<String, String>
     {
-        {"uint8","uint8_t" },
-        {"uint16","uint16_t" },
-        {"uint32","uint32_t" },
-        {"uint64","uint64_t" },
         {"int8","int8_t" },
         {"int16","int16_t" },
         {"int32","int32_t" },
         {"int64","int64_t" },
         {"vec", "std::vector"},
         {"map", "std::map"},
-        {"str", "std::string"},
-        { "string", "std::string"}
+        {"str", "std::string"}
      };
+
+    internal Dictionary<String, String> includeMap = new Dictionary<String, String>
+    {
+        {"std::vector", "vector"},
+        {"std::string", "string"},
+        {"std::map", "map"}
+    };
 
     internal string commentCharacter = "//";
     public CppClassGenerator()
@@ -37,12 +40,14 @@ public class CppClassGenerator : LanguageInterface
 
     internal bool GenerateHeader(string output_dir, string className, List<String> includes, Dictionary<String, String> members, Dictionary<String, Method> methods)
     {
+        HashSet<String> convertedIncludes = new HashSet<String>();
         string output = "";
         Directory.CreateDirectory(output_dir + "/cpp");
         string file_path = output_dir + "/cpp/" + className + ".h";
+        string includeString = "";
         foreach (string include in includes)
         {
-            output += $"#include<{include}.h\n";
+            includeString += $"#include \"{include}.h\"\n";
         }
 
         output += "namespace RPGTemplate\n{\n";
@@ -58,21 +63,46 @@ public class CppClassGenerator : LanguageInterface
                 output += commentCharacter + meth.Value.comment + "\n";
             }
             var returnVal = meth.Value.returnVal;
-            var containing = valueConversions.Keys.Where(key => returnVal.Contains(key));
-            foreach (var change in containing)
+            foreach (var key in valueConversions.Keys)
             {
-                returnVal.Replace(change, valueConversions[change]);
+                if (returnVal.Contains(key))
+                {
+                    returnVal = returnVal.Replace(key, valueConversions[key]);
+                    if (includeMap.ContainsKey(valueConversions[key]) && !convertedIncludes.Contains(valueConversions[key]))
+                    {
+                        includeString += $"#include <{includeMap[valueConversions[key]]}>\n";
+                        convertedIncludes.Add(valueConversions[key]);
+                    }
+                }
+                if (includeMap.ContainsKey(key) && !convertedIncludes.Contains(key))
+                {
+                    includeString += $"#include <{includeMap[key]}>\n";
+                        convertedIncludes.Add(key);
+                }
             }
+
             string paramString = "";
             foreach (var arg in meth.Value.paramList)
             {
                 var argVal = arg.Value;
-                var argContaining = valueConversions.Keys.Where(key => argVal.Contains(key));
-                foreach (var change in argContaining)
+                foreach (var key in valueConversions.Keys)
                 {
-                    returnVal.Replace(change, valueConversions[change]);
+                    if (argVal.Contains(key))
+                    {
+                        argVal = argVal.Replace(key, valueConversions[key]);
+                        if (includeMap.ContainsKey(valueConversions[key]) && !convertedIncludes.Contains(valueConversions[key]))
+                        {
+                            includeString += $"#include <{includeMap[valueConversions[key]]}>\n";
+                            convertedIncludes.Add(valueConversions[key]);
+                        }
+                    }
+                    if (includeMap.ContainsKey(key) && !convertedIncludes.Contains(key))
+                    {
+                        includeString += $"#include <{includeMap[key]}>\n";
+                        convertedIncludes.Add(key);
+                    }
                 }
-                paramString += $"{argVal} {arg.Key}";
+                paramString += $"const {argVal}& {arg.Key}";
                 if (!arg.Equals(meth.Value.paramList.Last()))
                 {
                     paramString += ", ";
@@ -89,18 +119,32 @@ public class CppClassGenerator : LanguageInterface
         foreach (var member in members)
         {
             var memberType = member.Value;
-            var containing = valueConversions.Keys.Where(key => memberType.Contains(key));
-            foreach (var change in containing)
+            foreach (var key in valueConversions.Keys)
             {
-                memberType.Replace(change, valueConversions[change]);
+                if (memberType.Contains(key))
+                {
+                    memberType = memberType.Replace(key, valueConversions[key]);
+                    if (includeMap.ContainsKey(valueConversions[key]) && !convertedIncludes.Contains(valueConversions[key]))
+                    {
+                        includeString += $"#include <{includeMap[valueConversions[key]]}>\n";
+                        convertedIncludes.Add(valueConversions[key]);
+                    }
+                }
+                if (includeMap.ContainsKey(key) && !convertedIncludes.Contains(key))
+                {
+                    includeString += $"#include <{includeMap[key]}>\n";
+                    convertedIncludes.Add(key);
+                }
             }
             getSets += $"const {memberType}& get{char.ToUpper(member.Key[0]) + member.Key.Substring(1)}() const;\n";
-            getSets += $"void set{char.ToUpper(member.Key[0]) + member.Key.Substring(1)}(const {memberType} val&);\n";
+            getSets += $"void set{char.ToUpper(member.Key[0]) + member.Key.Substring(1)}(const {memberType}& val);\n";
             memberVars += $"{memberType} {member.Key};\n";
         }
         output += getSets + "\nprivate:\n" + memberVars;
 
         // End members        
+        includeString += "#include <cstdint>\n\n";
+        output = includeString + output;
 
         output += "};\n"; // Class
         output += "}\n"; // Namespace
